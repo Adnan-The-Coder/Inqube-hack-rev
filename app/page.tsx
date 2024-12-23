39fddde0bfbb83a9475dced4aacddfd50d1f7724
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-12-23 13:49:23 +0530

    INQUBE

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-export default function Home() {
+import React from "react";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+
+export default function TextHoverEffectDemo() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center text-center">
-      <p className="animate-gradient bg-gradient-to-r from-[#a000c8] to-[#4c00b0] bg-300% bg-clip-text text-4xl font-bold text-transparent md:text-5xl md:leading-relaxed">
-        Coming soon
-      </p>
+    <div className="h-screen flex items-center justify-center">
+      <TextHoverEffect text="INQUBE" />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,27 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
-export default {
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -14,5 +30,7 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [addVariablesForColors],
+};
+
+export default config;
