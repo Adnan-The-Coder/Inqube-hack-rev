d543e76f131d15f8c148371f413f9ab0ef68100a
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2024-12-25 00:27:12 +0530

    bg-space

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 import React from "react";
-import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { SparklesCore } from "@/components/ui/Sparkles";
 
-export default function TextHoverEffectDemo() {
+export default function SparklesPreview() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <TextHoverEffect text="INQUBE" />
+    <div className="h-[43rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+      <h1 className="md:text-7xl font-mono text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+       Inqube Coming Soon
+      </h1>
     </div>
   );
 }
