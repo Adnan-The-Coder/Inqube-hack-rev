a55f0069188b37522c39bcd468d55a16aac4bcdd
Adnan <adnanali23189@gmail.com>
2024-12-26 17:35:24 +0530

    use Image from Next/Image in Additional.tsx file

--- components/Additional.tsx
+++ components/Additional.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Additional = () => {
@@ -15,14 +16,14 @@ const Additional = () => {
                     Our platform simplifies connecting with investors, offering resources and guidance to accelerate your startup growth.
                     </p>
                 </div>
-                <img src="https://img.icons8.com/?size=100&id=61&format=png&color=FFFFFF" alt='arrow' className='cursor-pointer w-[50px] border hover:bg-[#76b900] transition-all ease-out duration-200 border-white rounded-full p-3'/>
+                <Image src="https://img.icons8.com/?size=100&id=61&format=png&color=FFFFFF" alt='arrow' className='cursor-pointer w-[50px] border hover:bg-[#76b900] transition-all ease-out duration-200 border-white rounded-full p-3'/>
             </div>
         </div>
         <hr className='border border-[#505050] w-full md:w-[75rem]'/>
         <div className='flex flex-col md:flex-row items-center justify-between w-full md:w-[60rem] gap-4'>
             <div className='flex flex-col gap-4 text-center md:text-left'>
                 <h1 className='text-4xl md:text-7xl text-gray-300 font-thin'>70%</h1>
-                <p className='text-lg md:text-base'>Fuel Your Startup's Potential</p>
+                <p className='text-lg md:text-base'>Fuel Your Startup`s Potential</p>
             </div>
             <div className='flex flex-col md:flex-row gap-5 items-center justify-center md:justify-start'>
                 <div className='flex flex-col items-center gap-3'>
@@ -31,7 +32,7 @@ const Additional = () => {
                     Find investors who align with your vision and access valuable advice and resources to grow your startup.
                     </p>
                 </div>
-                <img src="https://img.icons8.com/?size=100&id=61&format=png&color=FFFFFF" alt='arrow' className='cursor-pointer w-[50px] border hover:bg-[#76b900] transition-all ease-out duration-200 border-white rounded-full p-3'/>
+                <Image src="https://img.icons8.com/?size=100&id=61&format=png&color=FFFFFF" alt='arrow' className='cursor-pointer w-[50px] border hover:bg-[#76b900] transition-all ease-out duration-200 border-white rounded-full p-3'/>
             </div>
         </div>
     </div>
