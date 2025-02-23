097c1b55bf5f5622f7d9d3f0fce4ae27100fd193
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2025-02-23 11:13:41 +0530

    team page changes

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -23,9 +23,36 @@ interface TeamMember {
 }
 
 const teamMembers: TeamMember[] = [
-    {
+  {
+    name: "Syed Adnan Ali",
+    role: "CEO",
+    image: "/assets/team/adnan.jpg",
+    about:
+    "Syed Adnan Ali is a versatile full-stack developer with a strong grasp of both client-side and server-side technologies. He has experience in building end-to-end web applications, from designing interactive user interfaces to developing scalable back-end systems. His technical expertise includes JavaScript, Node.js, Express, MongoDB, and React, which allows him to deliver feature-rich, performant applications. Adnan has a passion for problem-solving and optimizing code to improve efficiency. With his holistic approach to development, he excels at creating seamless integrations between front-end and back-end systems, ensuring a smooth user experience across the entire application.",
+    socials: {
+        linkedin: "https://linkedin.com/in/syedadnanali99/",
+        twitter: "https://x.com",
+        instagram: "https://instagram.com",
+        github: "https://github.com/Adnan-The-Coder",
+    },
+  },  
+  {
+    name: "Nazish Ahmed",
+    role: "CTO",
+    image:
+      "/assets/team/nazish.jpg",
+    about:
+      "Nazish Ahmed is a passionate full-stack developer with a keen focus on delivering scalable, high-performance, and user-centric web applications. With extensive experience working across both the front-end and back-end, Nazish has a deep understanding of web technologies such as JavaScript, Node.js, React, and more. His approach combines technical expertise with a strong emphasis on user experience and efficiency, ensuring seamless integration of front-end and back-end services. Nazish is dedicated to continuous learning and growth, constantly exploring new technologies to stay ahead in the fast-evolving field of web development.",
+    socials: {
+      linkedin: "https://linkedin.com",
+      twitter: "https://x.com",
+      instagram: "https://instagram.com",
+      github: "https://github.com",
+    },
+  },
+  {
       name: "Afzal Hashmi Syed",
-      role: "Team Lead",
+      role: "COO",
       image: "/assets/team/afzal.jpg",
       about:
         "Afzal Hashmi Syed is an experienced leader in the tech industry, bringing over two years of successful team leadership and project management. With a strong background in driving innovation, Afzal has led cross-functional teams to deliver large-scale projects on time and within budget. His leadership approach is centered on fostering collaboration, driving continuous improvement, and solving complex challenges. Afzal is passionate about leveraging technology to create impactful solutions and empowering teams to reach their full potential.",
@@ -37,35 +64,8 @@ const teamMembers: TeamMember[] = [
       },
     },
     {
-      name: "Nazish Ahmed",
-      role: "Full Stack Developer",
-      image:
-        "/assets/team/nazish.jpg",
-      about:
-        "Nazish Ahmed is a passionate full-stack developer with a keen focus on delivering scalable, high-performance, and user-centric web applications. With extensive experience working across both the front-end and back-end, Nazish has a deep understanding of web technologies such as JavaScript, Node.js, React, and more. His approach combines technical expertise with a strong emphasis on user experience and efficiency, ensuring seamless integration of front-end and back-end services. Nazish is dedicated to continuous learning and growth, constantly exploring new technologies to stay ahead in the fast-evolving field of web development.",
-      socials: {
-        linkedin: "https://linkedin.com",
-        twitter: "https://x.com",
-        instagram: "https://instagram.com",
-        github: "https://github.com",
-      },
-    },
-    {
-        name: "Syed Adnan Ali",
-        role: "Full Stack Developer",
-        image: "/assets/team/adnan.jpg",
-        about:
-        "Syed Adnan Ali is a versatile full-stack developer with a strong grasp of both client-side and server-side technologies. He has experience in building end-to-end web applications, from designing interactive user interfaces to developing scalable back-end systems. His technical expertise includes JavaScript, Node.js, Express, MongoDB, and React, which allows him to deliver feature-rich, performant applications. Adnan has a passion for problem-solving and optimizing code to improve efficiency. With his holistic approach to development, he excels at creating seamless integrations between front-end and back-end systems, ensuring a smooth user experience across the entire application.",
-        socials: {
-            linkedin: "https://linkedin.com/in/syedadnanali99/",
-            twitter: "https://x.com",
-            instagram: "https://instagram.com",
-            github: "https://github.com/Adnan-The-Coder",
-        },
-    },
-    {
         name: "Aayan Sayed",
-        role: "Frontend Engineer",
+        role: "Co-Founder",
         image: "/assets/team/aayan.jpg",
         about:
         "Aayan Sayed is a seasoned front-end engineer with a specialization in building highly interactive, visually appealing, and user-friendly interfaces. He combines his expertise in HTML, CSS, and JavaScript with modern frameworks like React and Vue to create smooth and responsive web experiences. Aayan is passionate about crafting web applications that not only look great but are also intuitive and easy to navigate. His focus on design principles and user experience ensures that every project he works on provides seamless usability while adhering to high performance and accessibility standards.",
@@ -78,7 +78,7 @@ const teamMembers: TeamMember[] = [
 },
 {
   name: "Mudassir Ahmed",
-  role: "ML Engineer",
+  role: "Co-Founder",
   image: "/assets/team/mudassir.jpg",
   about:
     "Mudassir Ahmed is an accomplished Machine Learning Engineer with a robust background in AI model development, data science, and automation. His expertise spans across designing, implementing, and optimizing machine learning models, as well as working with big data to derive actionable insights. Mudassir is adept at using various machine learning frameworks and tools, including TensorFlow, Scikit-learn, and PyTorch, to build intelligent systems that can automate processes and enhance decision-making. With a keen interest in artificial intelligence, he continuously seeks innovative ways to leverage machine learning to drive business success and technological advancement.",
@@ -91,7 +91,7 @@ const teamMembers: TeamMember[] = [
 },
     {
       name: "Haseeb Rahman",
-      role: "Support Specialist",
+      role: "Speaker",
       image: "/assets/team/haseeb.jpg",
       about:
         "Haseeb Rahman is a dedicated and results-driven Support Specialist, known for providing exceptional customer service and technical support. With an in-depth understanding of the company's products and services, Haseeb excels at troubleshooting issues, guiding customers through problem-solving processes, and ensuring that users receive the best possible experience. His approach emphasizes clear communication, empathy, and efficiency, ensuring prompt resolution of customer queries and issues. Haseeb is always focused on delivering high-quality support, enhancing user satisfaction, and maintaining strong customer relationships.",
